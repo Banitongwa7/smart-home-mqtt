8b0bbd52e6b822bc03caafa98e53e905736bd173
Banitongwa David <davidbanitongwa@gmail.com>
2023-05-19 18:28:09 +0100

    update devices.py

--- devices.py
+++ devices.py
@@ -39,3 +39,26 @@ class LightBulb(SmartDevice):
     def set_color(self, color):
         self.color = color
         self.notify(self.color_topic, color)
+
+
+class RollerBlind(SmartDevice):
+    def __init__(self, mqtt_client, name):
+        SmartDevice.__init__(self, mqtt_client, name)
+        self.color = '#000000'
+        self.state = False
+        self.setstate_topic ="home/{}/{}/setstate".format(APPLICATION_KEY, self.name)
+        self.state_topic = "home/{}/{}/state".format(APPLICATION_KEY,self.name)
+        self.setcolor_topic ="home/{}/{}/setcolor".format(APPLICATION_KEY, self.name)
+        self.color_topic = "home/{}/{}/color".format(APPLICATION_KEY,self.name)
+
+    def register_setstate(self, on_setstate):
+        self.client.subscribe(self.setstate_topic)
+        self.client.message_callback_add(self.setstate_topic,on_setstate)
+
+    def register_setcolor(self, on_setcolor):
+        self.client.subscribe(self.setcolor_topic)
+        self.client.message_callback_add(self.setcolor_topic,on_setcolor)
+    
+    def set_state(self, state):
+        self.state = state
+        self.notify(self.state_topic, state)
